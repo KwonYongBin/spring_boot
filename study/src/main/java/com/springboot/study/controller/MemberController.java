51881658b8fdd0b34a398f64f70ae8c46465709d
KwonYongBin <dydqls6789@naver.com>
2025-10-14 14:16:15 +0900

    20251014

--- study/src/main/java/com/springboot/study/controller/MemberController.java
+++ study/src/main/java/com/springboot/study/controller/MemberController.java
@@ -1,11 +1,10 @@
 package com.springboot.study.controller;
 
+import com.springboot.study.dto.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
@@ -30,13 +29,9 @@ public class MemberController {
     */
 
     @PostMapping("/login")
-    public String login( //login overriding
-            @RequestParam String id,
-            @RequestParam String pass,
-            Model model
-    ){
+    public String login( Member member, Model model ){
         String result = "";
-        if(id.equals("test") && pass.equals("1234")) result = "로그인 성공!!";
+        if(member.getId().equals("test") && member.getPass().equals("1234")) result = "로그인 성공!!";
         else result = "로그인 실패" ;
 
         model.addAttribute("result", result);
@@ -47,5 +42,11 @@ public class MemberController {
     @GetMapping("/signup")
     public String signup(){
         return "signup"; //회원가입
+    }
+
+    @PostMapping("/signup")
+    public String signup(Member member, Model model) {
+        model.addAttribute("member", member);
+        return "signupResult";
     }
 }

81037b67cd4d428b6418d7898dbb59b0688ffd68
KwonYongBin <dydqls6789@naver.com>
2025-10-17 17:01:25 +0900

    20251017

--- shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/controller/MemberController.java
+++ shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/controller/MemberController.java
@@ -1,12 +1,24 @@
 package com.springboot.shoppy_fullstack_app.controller;
 
 import com.springboot.shoppy_fullstack_app.dto.Member;
+import com.springboot.shoppy_fullstack_app.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
 @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 public class MemberController {
+
+    //서비스 객체 가져오기
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+
     
     //로그인
     @PostMapping("/login")
@@ -20,20 +32,17 @@ public class MemberController {
     //회원가입
     @PostMapping("/signup")
     public boolean signup(@RequestBody Member member) {
-        boolean result = true;
-        System.out.println(member.getId());
-        System.out.println(member.getPwd());
-        System.out.println(member.getName());
-        System.out.println(member.getPhone());
-        System.out.println(member.getEmail());
-
+        boolean result = false;
+        int rows = memberService.signup(member);
+        if(rows == 1) result = true;
         return result;
+
     }
 
     //회원가입 시 계정 중복 체크
     @PostMapping("/idcheck")
     public String idcheck(@RequestBody Member member) {
-        boolean result = false; // 아이도 존재 시 1, 존재하지 않으면 0
+        boolean result = memberService.idCheck(member.getId()); // 아이도 존재 시 1, 존재하지 않으면 0
         String msg = "";
         if(result) msg = "이미 존재하는 아이디 입니다.";
         else msg = "사용 가능한 아이디 입니다.";
---
+++ shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.shoppy_fullstack_app.repository;
+
+import com.springboot.shoppy_fullstack_app.dto.Member;
+
+public interface MemberRepository {
+    int save(Member member);
+
+    Long findById(String id);
+}

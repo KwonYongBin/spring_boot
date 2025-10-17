746e2ab7efd9c5efaf5a2600659e81da9c75afed
KwonYongBin <dydqls6789@naver.com>
2025-10-17 11:22:13 +0900

    20251017

--- shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/controller/MemberController.java
+++ shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/controller/MemberController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 public class MemberController {
     
     //로그인
--- shoppy-fullstack-app/frontend/src/feature/auth/authAPI.js
+++ shoppy-fullstack-app/frontend/src/feature/auth/authAPI.js
@@ -1,33 +1,48 @@
 import { login, logout } from '../../feature/auth/authSlice.js';
-import { validateFormCheck } from '../../utils/validate.js';
+import { validateFormCheck,  validateSignupFormCheck } from '../../utils/validate.js';
 import { axiosPost } from '../../utils/dataFetch.js';
 
+/**
+ Id 중복 체크
+ */
+export const getIdCheck = (id) => async(dispatch) => {
+    const data = { "id": id };
+    const url = "http://localhost:8080/member/idcheck";
+    const result = await axiosPost(url, data);
+    return result;
+}
 
+/**
+ Signup
+ */
+export const getSignup = (formData, param) => async(dispatch) => {
+    let result = null;
+    if(validateSignupFormCheck(param)) {
+        const url = "http://localhost:8080/member/signup";
+        result = await axiosPost(url, formData);
+    }
+    return result;
+}
+
+/**
+ Login
+ */
 export const getLogin = (formData, param) => async(dispatch) => {
 
     if(validateFormCheck(param)) {
-        /**
-            SpringBoot - @RestController, @PostMapping("/member/login")로 데이터를 넘겨야 함
-            axios api - post로 데이터를 넘기는 방법
-            axios({ 사용 방법
-                method: 'post',
-                url: '/user/12345',
-                data: {
-                firstName: 'Fred',
-                lastName: 'Flintstone'
-                }
-            })
-         */
         const url = "http://localhost:8080/member/login";
         const result = await axiosPost(url, formData);
         if(result) {
-            dispatch(login({"userId":formData.id}));   
-            return true;          
-        } 
+            dispatch(login({"userId":formData.id}));
+            return true;
+        }
     }
     return false;
 }
 
+/**
+ Logout
+ */
 export const getLogout = () => async(dispatch) => {
     dispatch(logout());
     return true;

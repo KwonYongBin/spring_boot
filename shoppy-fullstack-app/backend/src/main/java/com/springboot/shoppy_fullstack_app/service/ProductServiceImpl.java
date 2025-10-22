ccddb687ae82f41538a399fcdc659b62b10da25a
KwonYongBin <dydqls6789@naver.com>
2025-10-22 16:07:25 +0900

    20251022

--- shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/service/ProductServiceImpl.java
+++ shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/service/ProductServiceImpl.java
@@ -1,6 +1,9 @@
 package com.springboot.shoppy_fullstack_app.service;
 
 import com.springboot.shoppy_fullstack_app.dto.Product;
+import com.springboot.shoppy_fullstack_app.dto.ProductDetailinfo;
+import com.springboot.shoppy_fullstack_app.dto.ProductQna;
+import com.springboot.shoppy_fullstack_app.dto.ProductReturn;
 import com.springboot.shoppy_fullstack_app.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +13,24 @@ import java.util.List;
 @Service
 //@Transactional
 public class ProductServiceImpl implements ProductService {
+    @Override
+    public ProductReturn findRetrun() {return productRepository.findReturn();}
+
     private ProductRepository productRepository;
 
     @Autowired
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<ProductQna> findQna(int pid) {
+        return productRepository.findQna(pid);
+    }
+
+    @Override
+    public ProductDetailinfo findDetailinfo(int pid) {
+        return productRepository.findProductDetailinfo(pid);
     }
 
     @Override
--- shoppy-fullstack-app/frontend/src/feature/Product/ProductAPI.js
+++ shoppy-fullstack-app/frontend/src/feature/Product/ProductAPI.js
@@ -2,6 +2,36 @@ import React from 'react';
 import { createProduct, filterProduct } from './productSlice.js';
 import { axiosData, groupByRows, axiosGet,  axiosPost } from '../../utils/dataFetch.js';
 
+/**
+ 상품 Return
+ */
+export const getReturn = async() => {
+    const url = "/product/return";
+    const returnData = await axiosGet(url);
+    const list = JSON.parse(returnData.list);
+    return {...returnData, list: list};
+}
+
+/**
+    상품 QnA
+*/
+export const getQna = async(pid) => {
+    const url = "/product/qna";
+    const qna  = await axiosPost(url, {"pid": pid});
+    return qna;
+}
+
+
+/**
+    상품 상세 정보
+*/
+export const getDetailinfo = async(pid) => {
+    const url = "/product/detailinfo";
+    const info  = await axiosPost(url, {"pid": pid});
+    const list = JSON.parse(info.list);
+    return { ...info, list: list };
+}
+
 export const getProduct = (pid) => async(dispatch) => {
     // dispatch(filterProduct(pid));
     const url = "/product/pid";
